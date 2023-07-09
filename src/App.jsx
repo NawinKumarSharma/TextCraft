6c2a31b70b07ea557e79ea53a86e1c7f87a8b078
NawinKumarSharma <nawinsharma60@gmail.com>
2023-07-09 23:13:27 +0530

    fix: refactor imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
 import { useState } from 'react';
 import './App.css';
 import React from 'react';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
+import About from './components/About.jsx';
+import Navbar from './components/Navbar.jsx';
+import TextForm from './components/TextForm.jsx';
+import Alert from './components/Alert.jsx';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(()=>{
-        setAlert(null)
-      }, 1500);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled","success");
+      showAlert("Dark mode has been enabled", "success");
       // document.title = 'TextCraft-Dark Mode'
 
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled","success");
+      showAlert("Light mode has been enabled", "success");
       // document.title = 'TextCraft-Light Mode'
 
 
@@ -40,34 +40,34 @@ function App() {
     }
   }
 
-return (
-  <>
-    <BrowserRouter>
-      <Navbar
-        title="TextCraft2"
-        aboutText="TextAbouts"
-        mode={mode}
-        toggleMode={toggleMode}
-      />
-      <Alert alert={alert} />
-      <div className="container my-4" mode={mode}>
-        <Routes>
-          <Route exact path="/about" element={<About mode={mode}/>}></Route>
-          <Route
-            exact path="/"
-            element={
-              <TextForm
-                showAlert={showAlert}
-                heading="Enter Text to analyze "
-                mode={mode}
-              />
-            }
-          ></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  </>
-);
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar
+          title="TextCraft2"
+          aboutText="TextAbouts"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-4" mode={mode}>
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} />}></Route>
+            <Route
+              exact path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter Text to analyze "
+                  mode={mode}
+                />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
